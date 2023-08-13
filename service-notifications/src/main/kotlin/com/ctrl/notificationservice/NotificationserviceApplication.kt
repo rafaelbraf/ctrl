e2c72f34c7a2687f568e0ed9eeaf14f97231b19a
rafaelbraf <rafabrunofelix@hotmail.com>
2023-08-13 19:55:19 -0300

    subindo serviço de notificações

---
+++ service-notifications/src/main/kotlin/com/ctrl/notificationservice/NotificationserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.ctrl.notificationservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotificationserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotificationserviceApplication>(*args)
+}
---
+++ service-notifications/src/test/kotlin/com/ctrl/notificationservice/NotificationserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ctrl.notificationservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotificationserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
